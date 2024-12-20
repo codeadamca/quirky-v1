5430d93940219b9f68588688baf08b906faaf860
Adam Thomas <thomasadam83@hotmail.com>
2024-12-20 13:10:03 -0500

    Added gitignore

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 import express from "express";
 import path from "path";
-import * as url from 'url';
+import * as url from "url";
 import multer from "multer";
 //import fs from "fs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import flash from "connect-flash";
 import bcrypt from "bcrypt";
-import { v4 as uuidv4 } from 'uuid';
-import users from './public/module/users.js'; 
-import projects from './public/module/projects.js'; 
+import { v4 as uuidv4 } from "uuid";
+import users from "./public/module/users.js";
+import projects from "./public/module/projects.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 import { MongoClient, ObjectId } from "mongodb";
-const _dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const _dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-//MONGODB CLIENT SETUP
-const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_ATLAS}`;
+// MONGODB CLIENT SETUP
+const dbUrl = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}`;
 
-// const dbUrl = "mongodb+srv://testdbuser:KqqIRLt1aZyFBY5X@cluster0.sckq1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
-const client = new MongoClient(dbUrl); 
+const client = new MongoClient(dbUrl);
 const app = express();
 const port = process.env.PORT || "5214";
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/img')
+    cb(null, "public/img");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-var upload = multer({ storage: storage })
+    cb(null, file.originalname);
+  },
+});
+var upload = multer({ storage: storage });
 
-app.use(express.static('public/img'));
+app.use(express.static("public/img"));
 
 // set up Expess to use pug as a template engine
 app.set("views", path.join(_dirname, "templates"));
@@ -53,11 +52,12 @@ app.use(
     saveUninitialized: true,
     cookie: { secure: false, maxAge: oneDay },
     resave: false,
-  }));
+  })
+);
 
 app.use((req, res, next) => {
-  app.locals.userSession = req.session.user || null; 
-  console.log("Current user session: ", req.session.user); 
+  app.locals.userSession = req.session.user || null;
+  console.log("Current user session: ", req.session.user);
   next();
 });
 
@@ -83,21 +83,23 @@ app.post("/login/submit", async (req, res) => {
   let userResult = await users.findOneByemail(email);
   console.log("login submit");
   if (userResult) {
-    const verifyValue = await users.verifyPassword(password, userResult.password);
+    const verifyValue = await users.verifyPassword(
+      password,
+      userResult.password
+    );
     if (verifyValue) {
       const sessionId = uuidv4();
       userResult.sessionId = sessionId;
       await userResult.save();
       res.cookie("sessionId", sessionId, { maxAge: oneDay, httpOnly: true });
       req.session.user = {
-        userId: userResult._id, 
+        userId: userResult._id,
         username: userResult.username,
         role: userResult.role,
         email: userResult.email,
-      };     
-      // console.log("Session after login: ", req.session.user); 
-      if(req.session.user.role)
-      return res.redirect("/admin/project");
+      };
+      // console.log("Session after login: ", req.session.user);
+      if (req.session.user.role) return res.redirect("/admin/project");
     } else {
       req.flash("error", "Password incorrect");
       return res.redirect("/login");
@@ -146,39 +148,45 @@ app.post("/register", async (req, res) => {
 
 app.get("/api/projects", async (req, res) => {
   let projList = await projects.getProj();
-  res.json(projList); 
+  res.json(projList);
 });
 
 //http://localhost:5214
 app.get("/", async (req, res) => {
-  let userSession=req.session.user || null;
+  let userSession = req.session.user || null;
   console.log(userSession);
   let projList = await projects.getProj();
-  if (!projList.length&&(userSession)) {
-    const initializeProj= await projects.initializeProjects(userSession.userId);
+  if (!projList.length && userSession) {
+    const initializeProj = await projects.initializeProjects(
+      userSession.userId
+    );
     console.log(initializeProj);
     projList = await projects.getProj();
   }
   console.log("Projects to render:", projList);
-  res.render("index", { title: "Home", directory: projList, userSession: req.session.user || null });
+  res.render("index", {
+    title: "Home",
+    directory: projList,
+    userSession: req.session.user || null,
+  });
 });
 
 // ADMIN - Project
 app.get("/admin/project", async (req, res) => {
   console.log("Session Data:", req.session.user);
   if (!req.session.user) {
-    return res.redirect("/login"); 
+    return res.redirect("/login");
   }
   let projList = await projects.getProj();
-  console.log("admin",req.session.user);
+  console.log("admin", req.session.user);
   if (!projList.length) {
     await projects.initializeProjects(req.session.user.userId);
     projList = await projects.getProj();
   }
-  console.log("initialprojects:",projList);
-  console.log("users",req.session.user);
+  console.log("initialprojects:", projList);
+  console.log("users", req.session.user);
   let title = "Administer Directory Projects";
-  if(req.session.user.role==="user"){  
+  if (req.session.user.role === "user") {
     projList = await projects.getUserProj(req.session.user.userId);
     title = "My Projects";
   }
@@ -189,97 +197,118 @@ app.get("/admin/project", async (req, res) => {
     return proj;
   });
   console.log(projList);
-  res.render("proj-list", { title: title, directory: formatProjList, userSession: req.session.user || null });
+  res.render("proj-list", {
+    title: title,
+    directory: formatProjList,
+    userSession: req.session.user || null,
+  });
 });
 
 app.get("/admin/project/add", async (req, res) => {
   console.log("Session Data:", req.session.user);
   if (!req.session.user) {
-    return res.redirect("/login"); 
+    return res.redirect("/login");
   }
   let projList = await projects.getProj();
   if (!projList.length) {
     await projects.initializeProjects(req.session.user.id);
     projList = await projects.getProj();
   }
-  res.render("proj-add", { title: "Add New Project", directory: projList, userSession: req.session.user || null});
-})
+  res.render("proj-add", {
+    title: "Add New Project",
+    directory: projList,
+    userSession: req.session.user || null,
+  });
+});
 
-app.post("/admin/project/add/submit", upload.single('screen'),async (req, res) => {
-  console.log("form",req.body);
-  console.log("Session Data:", req.session.user);
-  if (!req.session.user) {
-    return res.redirect("/login"); 
+app.post(
+  "/admin/project/add/submit",
+  upload.single("screen"),
+  async (req, res) => {
+    console.log("form", req.body);
+    console.log("Session Data:", req.session.user);
+    if (!req.session.user) {
+      return res.redirect("/login");
+    }
+    let newProj = {
+      website_name: req.body.website_name,
+      Github_username: req.body.Github_username,
+      URL: req.body.URL,
+      GitHub_repo: req.body.GitHub_repo,
+      screen: req.file.filename,
+      date_added: new Date(),
+      createdBy: req.session.user.userId,
+      /*  number: req.body.projId */
+    };
+    await projects.addProj(newProj);
+    res.redirect("/admin/project");
   }
-  let newProj = {
-    website_name: req.body.website_name,
-    Github_username: req.body.Github_username,
-    URL: req.body.URL,
-    GitHub_repo: req.body.GitHub_repo,
-    screen: req.file.filename,
-    date_added: new Date(),
-    createdBy:req.session.user.userId,
-   /*  number: req.body.projId */
-  };
-  await projects.addProj(newProj);
-  res.redirect("/admin/project");
-})
+);
 
 app.get("/admin/project/delete", async (req, res) => {
   let id = req.query.projId;
   console.log("Session Data:", req.session.user);
   if (!req.session.user) {
-    return res.redirect("/login"); 
+    return res.redirect("/login");
   }
   await projects.deleteProj(id);
   res.redirect("/admin/project");
-})
+});
 
 app.get("/admin/project/edit", async (req, res) => {
   console.log("Session Data:", req.session.user);
   if (!req.session.user) {
-    return res.redirect("/login"); 
+    return res.redirect("/login");
   }
   if (req.query.projId) {
     let projToEdit = await projects.getSingleProj(req.query.projId);
     let projList = await projects.getProj();
-    res.render("proj-edit", { title: "Edit the Project", directory: projList, editProj: projToEdit, userSession: req.session.user || null });
-  }
-  else {
+    res.render("proj-edit", {
+      title: "Edit the Project",
+      directory: projList,
+      editProj: projToEdit,
+      userSession: req.session.user || null,
+    });
+  } else {
     res.redirect("/admin/project");
   }
 });
 
-app.post("/admin/project/edit/submit", upload.single('screen'),async (req, res) => { 
-  console.log("Session Data:", req.session.user);
-  if (!req.session.user) {
-    return res.redirect("/login"); 
-  }let id = req.body.projId; 
-  console.log(id);
-  let idFilter = { _id: new ObjectId(id)};
-  console.log(idFilter);
-  //get detailed form values and build a JSON object containing these (updated) values
-  let editScreen = req.file ? req.file.filename : req.body.screen;
-  let project = {
-    website_name: req.body.website_name,
-    Github_username: req.body.Github_username,
-    URL: req.body.URL,
-    GitHub_repo: req.body.GitHub_repo,
-    screen: editScreen,
-    // date_added: new Date(),
-    // createdBy:req.session.user.userId
-  };
-  //run editLink(idFilter, link) and await the result
-  console.log(idFilter);
-  await projects.editProj(idFilter, project);
-  res.redirect("/admin/project");
-})
- 
+app.post(
+  "/admin/project/edit/submit",
+  upload.single("screen"),
+  async (req, res) => {
+    console.log("Session Data:", req.session.user);
+    if (!req.session.user) {
+      return res.redirect("/login");
+    }
+    let id = req.body.projId;
+    console.log(id);
+    let idFilter = { _id: new ObjectId(id) };
+    console.log(idFilter);
+    //get detailed form values and build a JSON object containing these (updated) values
+    let editScreen = req.file ? req.file.filename : req.body.screen;
+    let project = {
+      website_name: req.body.website_name,
+      Github_username: req.body.Github_username,
+      URL: req.body.URL,
+      GitHub_repo: req.body.GitHub_repo,
+      screen: editScreen,
+      // date_added: new Date(),
+      // createdBy:req.session.user.userId
+    };
+    //run editLink(idFilter, link) and await the result
+    console.log(idFilter);
+    await projects.editProj(idFilter, project);
+    res.redirect("/admin/project");
+  }
+);
+
 // ADMIN - User
 app.get("/admin/user", async (req, res) => {
   console.log("Session Data:", req.session.user);
   if (!req.session.user) {
-    return res.redirect("/login"); 
+    return res.redirect("/login");
   }
   let userList = await users.getUsers();
   if (!userList.length) {
@@ -293,26 +322,34 @@ app.get("/admin/user", async (req, res) => {
     return user;
   });
   console.log(req.session.user);
-  res.render("user-list", { title: "Students in HTTP5214", students: formatUserList, userSession: req.session.user || null });
+  res.render("user-list", {
+    title: "Students in HTTP5214",
+    students: formatUserList,
+    userSession: req.session.user || null,
+  });
 });
 
 app.get("/admin/user/add", async (req, res) => {
   console.log("Session Data:", req.session.user);
   if (!req.session.user) {
-    return res.redirect("/login"); 
+    return res.redirect("/login");
   }
   let userList = await users.getUsers();
   if (!userList.length) {
     await users.initializeUsers();
     userList = await users.getUsers();
   }
-  res.render("user-add", { title: "Create a New Account", students: userList, userSession: req.session.user || null});
-})
+  res.render("user-add", {
+    title: "Create a New Account",
+    students: userList,
+    userSession: req.session.user || null,
+  });
+});
 
-app.post("/admin/user/add/submit", async (req, res) => {  
+app.post("/admin/user/add/submit", async (req, res) => {
   console.log("Session Data:", req.session.user);
   if (!req.session.user) {
-    return res.redirect("/login"); 
+    return res.redirect("/login");
   }
   const { username, email, password, role } = req.body;
   let userResult = await users.findOneByemail(email);
@@ -330,57 +367,57 @@ app.post("/admin/user/add/submit", async (req, res) => {
     });
   }
   await users.addUserAdmin(username, email, password, role);
-  res.redirect("/admin/user"); 
-})
+  res.redirect("/admin/user");
+});
 
 app.get("/admin/user/delete", async (req, res) => {
   console.log("Session Data:", req.session.user);
   if (!req.session.user) {
-    return res.redirect("/login"); 
+    return res.redirect("/login");
   }
   let id = req.query.userId;
   await users.deleteUser(id);
   res.redirect("/admin/user");
-})
+});
 
 app.get("/admin/user/edit", async (req, res) => {
   console.log("Session Data:", req.session.user);
   if (!req.session.user) {
-    return res.redirect("/login"); 
+    return res.redirect("/login");
   }
-    let userToEdit = await users.findOneById(req.query.userId);
-    let userList = await users.getUsers();
-    res.render("user-edit", { title: "Edit User", students: userList, editUser: userToEdit, userSession: req.session.user || null });
+  let userToEdit = await users.findOneById(req.query.userId);
+  let userList = await users.getUsers();
+  res.render("user-edit", {
+    title: "Edit User",
+    students: userList,
+    editUser: userToEdit,
+    userSession: req.session.user || null,
   });
+});
 
 app.post("/admin/user/edit/submit", async (req, res) => {
   console.log("Session Data:", req.session.user);
   if (!req.session.user) {
-    return res.redirect("/login"); 
+    return res.redirect("/login");
   }
-  let id = req.body.userId; 
+  let id = req.body.userId;
   console.log(id);
-  let idFilter = { _id: new ObjectId(id)};
+  let idFilter = { _id: new ObjectId(id) };
 
   let updatedUser = {
     username: req.body.username,
     email: req.body.email,
     role: req.body.role,
     password: req.body.password,
-    date_added: new Date()
+    date_added: new Date(),
   };
 
-  console.log(idFilter,updatedUser);
+  console.log(idFilter, updatedUser);
   await users.updateUser(idFilter, updatedUser);
   res.redirect("/admin/user");
-})
-
-
+});
 
 //set up server listening
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
-
-
-
--- public/module/users.js
+++ public/module/users.js
@@ -3,155 +3,155 @@ import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 dotenv.config();
 
-const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_ATLAS}`;
+const dbUrl = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}`;
 
 // console.log("users",dbUrl);
 
 const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    createdDate: { type: Date, default: Date.now },
+    sessionId: String,
+  },
+  {
+    collection: "users",
+  }
+);
+const Users = mongoose.model("users", UserSchema);
+
+async function connect() {
+  await mongoose.connect(dbUrl);
+}
+
+async function hashPassword(password) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
+
+async function verifyPassword(password, hashedPassword) {
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+  return isMatch;
+}
+
+async function initializeUsers() {
+  await connect();
+  const passFlora = await hashPassword("flora.123");
+  const passAdam = await hashPassword("adam.123");
+  const passTest = await hashPassword("test.123");
+  const userList = [
     {
-      username: { type: String, required: true },
-      role: {
-        type: String,
-        enum: ["admin", "user"],
-        default: "user",
-      },
-      email: { type: String, required: true },
-      password: { type: String, required: true },
-      createdDate: { type: Date, default: Date.now },
-      sessionId: String,
+      username: "Flora",
+      role: "admin",
+      email: "flora@123.com",
+      password: passFlora,
+      createdDate: new Date("2024-11-01"),
+      sessionId: "bvcdcegoier",
     },
     {
-      collection: "users",
-    }
-  );
-  const Users = mongoose.model("users", UserSchema);
+      username: "Adam",
+      role: "admin",
+      email: "adam@123.com",
+      password: passAdam,
+      createdDate: new Date("2024-11-01"),
+      sessionId: "bhimcegoier",
+    },
+    {
+      username: "test",
+      role: "user",
+      email: "test@123.com",
+      password: passTest,
+      createdDate: new Date("2024-11-01"),
+      sessionId: "bhimcikdier",
+    },
+  ];
+  await Users.insertMany(userList);
+}
 
-  async function connect() {
-    await mongoose.connect(dbUrl);
-  }
-  
-  async function hashPassword(password) {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    return hashedPassword;
-  }
-  
-  async function verifyPassword(password, hashedPassword) {
-    const isMatch = await bcrypt.compare(password, hashedPassword);
-    return isMatch;
-  }
-  
-  async function initializeUsers() {
-    await connect();
-    const passFlora = await hashPassword("flora.123");
-    const passAdam = await hashPassword("adam.123");
-    const passTest = await hashPassword("test.123");
-    const userList = [
-      {
-        username: "Flora",
-        role: "admin",
-        email: "flora@123.com",
-        password: passFlora,
-        createdDate: new Date("2024-11-01"),
-        sessionId: "bvcdcegoier",
-      },
-      {
-        username: "Adam",
-        role: "admin",
-        email: "adam@123.com",
-        password: passAdam,
-        createdDate: new Date("2024-11-01"),
-        sessionId: "bhimcegoier",
-      },
-      {
-        username: "test",
-        role: "user",
-        email: "test@123.com",
-        password: passTest,
-        createdDate: new Date("2024-11-01"),
-        sessionId: "bhimcikdier",
-      }
-    ];
-    await Users.insertMany(userList);
-  }
+async function findOneByemail(inputEmail) {
+  await connect();
+  return await Users.findOne({ email: inputEmail }).exec();
+}
 
-  async function findOneByemail(inputEmail) {
-    await connect();
-    return await Users.findOne({ email: inputEmail }).exec();
-  }
+async function findOneById(id) {
+  await connect();
+  const editId = { _id: new mongoose.Types.ObjectId(id) };
+  return await Users.findOne(editId);
+}
 
-  async function findOneById(id) {
-    await connect();
-    const editId = { _id: new mongoose.Types.ObjectId(id) };
-    return await Users.findOne(editId);
-  }
+async function addUser(pUsername, pEmail, pPassword) {
+  const hashedPass = await hashPassword(pPassword);
+  await connect();
+  let newUser = new Users({
+    username: pUsername,
+    email: pEmail,
+    password: hashedPass,
+    createdDate: Date.now(),
+  });
+  await Users.create(newUser);
+}
 
-  async function addUser(pUsername, pEmail, pPassword) {
-    const hashedPass = await hashPassword(pPassword);
-    await connect();
-    let newUser = new Users({
-      username: pUsername,
-      email: pEmail,
-      password: hashedPass,
-      createdDate: Date.now(),
-    });
-    await Users.create(newUser);
-  }
+async function addUserAdmin(pUsername, pEmail, pPassword, pRole) {
+  const hashedPass = await hashPassword(pPassword);
+  await connect();
+  let newUser = new Users({
+    username: pUsername,
+    email: pEmail,
+    password: hashedPass,
+    role: pRole,
+    createdDate: Date.now(),
+  });
+  await Users.create(newUser);
+}
 
-  async function addUserAdmin(pUsername, pEmail, pPassword, pRole) {
-    const hashedPass = await hashPassword(pPassword);
-    await connect();
-    let newUser = new Users({
-      username: pUsername,
-      email: pEmail,
-      password: hashedPass,
-      role: pRole,
-      createdDate: Date.now(),
-    });
-    await Users.create(newUser);
-  }
+async function getUsers() {
+  await connect();
+  return await Users.find({}).sort({ createdDate: -1 }); //return array for find all
+}
 
-  async function getUsers() {
-    await connect();
-    return await Users.find({}).sort({ createdDate: -1 }); //return array for find all
-  }
-
-  async function updateUser(filter, updatedUser) {
-    await connect();
-    let updUser = {
-      username: updatedUser.username,
-      email: updatedUser.email,
-      role: updatedUser.role,
-    };
-    if (updatedUser.password) {
-      const hashedPass = await hashPassword(updatedUser.password);
-      updUser.password = hashedPass;  
-    }  
-    return await Users.updateOne(filter, { $set: updUser });
-  }
-  
-  async function deleteUser(id) {
-    await connect();
-    let filter = { _id: new mongoose.Types.ObjectId(id) };
-    return await Users.deleteOne(filter);
-  }
-  
-  async function reset() {
-    await connect();
-    return await Users.deleteMany({});
-  }
-
-  const users = {
-    initializeUsers,
-    addUser,
-    updateUser,
-    getUsers,
-    deleteUser,
-    reset,
-    verifyPassword,
-    findOneByemail,
-    addUserAdmin,
-    findOneById,
-    UserSchema,
+async function updateUser(filter, updatedUser) {
+  await connect();
+  let updUser = {
+    username: updatedUser.username,
+    email: updatedUser.email,
+    role: updatedUser.role,
   };
-  export default users;
+  if (updatedUser.password) {
+    const hashedPass = await hashPassword(updatedUser.password);
+    updUser.password = hashedPass;
+  }
+  return await Users.updateOne(filter, { $set: updUser });
+}
+
+async function deleteUser(id) {
+  await connect();
+  let filter = { _id: new mongoose.Types.ObjectId(id) };
+  return await Users.deleteOne(filter);
+}
+
+async function reset() {
+  await connect();
+  return await Users.deleteMany({});
+}
+
+const users = {
+  initializeUsers,
+  addUser,
+  updateUser,
+  getUsers,
+  deleteUser,
+  reset,
+  verifyPassword,
+  findOneByemail,
+  addUserAdmin,
+  findOneById,
+  UserSchema,
+};
+export default users;
